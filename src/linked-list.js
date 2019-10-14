2bdbd430d7e3fde44511a37ac23e25163f8318ff
Stanislav Nikolaenko <grot_cs5@mail.ru>
2019-10-14 22:39:07 +0300

    fixed: insertAt function done

--- src/linked-list.js
+++ src/linked-list.js
@@ -23,11 +23,17 @@ class LinkedList {
     }
 
     head() {
-      return this._head.data;
+     if(this._head){
+        return this._head.data
+     }
+     return null;
     }
 
     tail() {
-      return this._tail.data;
+      if(this._tail){
+        return this._tail.data       
+      }
+      return null;
     }
 
     at(index) {
@@ -38,7 +44,40 @@ class LinkedList {
       return search_elem.data;
     }
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+      if(index < 0 || this.length < index){
+        return false;
+      }
+      let node = new Node(data);
+      
+      if(index === 0){              
+        node.next = this._head;
+        this._head.prev = node;       
+        this._head = node;        
+      }
+      else if(index === this.length){
+        this._tail.next = node;
+        node.prev = this._tail;
+        this._tail = node;
+        
+      }
+      else{
+        let start = this._head;
+        let prev = null;
+        let counter = 0;
+        while(counter < index){
+          prev = start;
+          start = start.next;
+          counter++;
+        }
+        prev.next =node;
+        node.prev = prev;
+        node.next = start;
+        start.prev = node;
+      }
+      this.length++;
+
+    }
 
     isEmpty() {
       if(this.length!==0){
@@ -55,6 +94,30 @@ class LinkedList {
     }
 
     deleteAt(index) {
+      if(index<0 || this.length<=index){
+        return null;
+      }
+      let search;
+      if(index ===0){
+        search = this._head;
+        this._head = this._head.next;
+        this._head.prev = null;
+      }
+      else if( index === this.length -1){
+        search = this._tail;
+        this._tail = this._tail.prev;
+        this._tail.next = null;
+      }
+      else{
+        let counter =0;
+        let start = this._head;
+        while(counter < index){
+          start = start.next;
+          counter ++
+        }
+        start.prev.next= start.next;
+        start.next.prev = start.prev;
+        }
       
     }
 
